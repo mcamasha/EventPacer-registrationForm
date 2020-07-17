8acd838e548742a86f6d787ed6c0d9baeac1810a
mcaMasha <mcaMasha@gmail.com>
2020-07-17 15:42:25 +0300

    Updates

---
+++ precache-manifest.91da489209f0e442f0969dd8ec1c53d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70fa6380e648a7b8df1a14553960abd",
+    "url": "/event-pacer-registration-form/index.html"
+  },
+  {
+    "revision": "6d1940614420f99479aa",
+    "url": "/event-pacer-registration-form/static/css/main.d0ac4ad1.chunk.css"
+  },
+  {
+    "revision": "2371f09bca113b417589",
+    "url": "/event-pacer-registration-form/static/js/2.850683df.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/event-pacer-registration-form/static/js/2.850683df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1940614420f99479aa",
+    "url": "/event-pacer-registration-form/static/js/main.12388a00.chunk.js"
+  },
+  {
+    "revision": "ea9f4a77723b57f884a9",
+    "url": "/event-pacer-registration-form/static/js/runtime-main.ccc69721.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/event-pacer-registration-form/precache-manifest.91da489209f0e442f0969dd8ec1c53d4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/event-pacer-registration-form/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
